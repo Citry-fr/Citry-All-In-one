774ba630eabe86ce816982291a01a0efd71616e7
Tristan <citrynox9@gmail.com>
2022-06-27 21:06:00 +0200

    Color Palette

--- Front/citry-aio/src/App.vue
+++ Front/citry-aio/src/App.vue
@@ -2,6 +2,10 @@
   <header class="header">
     <p class="header__p">Soon le bg</p>
     <div class="header__nav">
+      <router-link class="header__nav__link home" to="/"
+        >H
+        <div class="header__nav__border"></div
+      ></router-link>
       <router-link class="header__nav__link first" to="/notes"
         >Notes
         <div class="header__nav__border"></div
@@ -31,13 +35,15 @@ export default {
 @import '../node_modules/nes.css/css/nes.css';
 @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
 @import 'node_modules/pixel-borders/src/styles/pixel-borders.scss';
+@import './scss/variable.scss';
 
 * {
   font-family: 'Press Start 2P', cursive;
+  color: $text-primary;
 }
 
 body {
-  background-color: grey;
+  background-color: $bg-dark;
 }
 
 .header {
@@ -52,42 +58,53 @@ body {
   left: 50%;
   margin-left: -45%;
   padding: 5px 0 0 0;
-  background-color: white;
-  @include pixel-borders($corner-size: 2);
+  background-color: $bg-light;
+  @include pixel-borders($corner-size: 2, $border-color: $border-color);
   border-bottom-left-radius: 0 !important;
   border-bottom-right-radius: 0 !important;
   border-bottom: 0;
   &__p {
     margin: 10px 0;
+    color: $text-primary;
   }
   &__nav {
     margin-top: 10px;
     width: 100%;
     height: 40px;
-    border-bottom: 4px solid black;
+    border-bottom: 4px solid $border-color;
     display: flex;
-    justify-content: center;
+    justify-content: flex-start;
+    padding-left: 30px;
     align-items: center;
     &__link {
-      @include pixel-borders($corner-size: 2);
+      @include pixel-borders($corner-size: 2, $border-color: $border-color);
       border-bottom-left-radius: 0 !important;
       border-bottom-right-radius: 0 !important;
       border-top-right-radius: 0 !important;
       border-bottom: 0;
       border-right: 0;
-      background-color: white;
+      background-color: $bg-light;
+      color: $text-primary;
+      text-decoration: none;
       position: relative;
       bottom: -3px;
       width: 150px;
       display: flex;
       justify-content: center;
       padding: 5px 0px;
+      &:hover {
+        text-decoration: none;
+        color: $text-secondary;
+      }
     }
-    &__link + .second {
+    &__link + .first {
       left: -4px;
     }
-    &__link + .third {
+    &__link + .second {
       left: -8px;
+    }
+    &__link + .third {
+      left: -12px;
     }
     &__border {
       position: absolute;
@@ -95,8 +112,8 @@ body {
       right: 0px;
       width: 100%;
       height: 100%;
-      border-right: 4px solid black;
-      border-bottom: 4px solid black;
+      border-right: 4px solid $border-color;
+      border-bottom: 4px solid $border-color;
     }
     & a.router-link-active {
       padding: 10px 0px;
@@ -106,5 +123,8 @@ body {
       }
     }
   }
+}
+.home {
+  width: 50px;
 }
 </style>
--- Front/citry-aio/src/router/index.js
+++ Front/citry-aio/src/router/index.js
@@ -2,10 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Notes from '../views/NotesView.vue';
 import Counters from '../views/CountersView.vue';
 import Timers from '../views/TimersView.vue';
+import Home from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
     {
       path: '/notes',
       name: 'notes',
